51d6a83b9b5b763292bc91b398a213adcbfa3ed2
alik-p <aliaksandr.papko@gmaol.com>
2020-06-23 23:06:33 +0300

    refactor: inherit model `Asteroid` from `Mass`

--- src/game/asteroid/asteroid.model.ts
+++ src/game/asteroid/asteroid.model.ts
@@ -1,26 +1,22 @@
 import { AsteroidDrawing as Drawing } from './asteroid-drawing';
+import { Mass } from '../shared/mass/mass.model';
 
-export class Asteroid {
 
-    readonly #radius: number;
+export class Asteroid extends Mass {
+
     readonly #noise: number;
     readonly #shape: number[] = [];
-    #x: number;
-    #y: number;
-    #angle: number;
-    #speed: { x: number, y: number, rotation: number };
 
-    constructor(x: number, y: number, radius: number) {
-        this.#x = x;
-        this.#y = y;
-        this.#angle = 0;
-        this.#radius = radius;
+    constructor(protected x: number, protected y: number, protected radius: number) {
+        super(
+            x, y, radius, 0,
+            {
+                x: Math.random() * 100,
+                y: Math.random() * 50,
+                rotation: 2 * Math.PI * (Math.random() - 0.5),
+            }
+        );
         this.#noise = Math.random() - 0.4;
-        this.#speed = {
-            x: Math.random() * 100,
-            y: Math.random() * 50,
-            rotation: 2 * Math.PI * (Math.random() - 0.5),
-        };
         const segments = 5 + Math.random() * 30;
         for (let i = 0; i < segments; i++) {
             this.#shape.push(Math.random() - 0.5);
@@ -30,36 +26,11 @@ export class Asteroid {
 
     draw(context: CanvasRenderingContext2D): void {
         context.save();
-        context.translate(this.#x, this.#y);
-        context.rotate(this.#angle);
-        Drawing.drawAsteroid(context, this.#radius, this.#shape, this.#noise);
+        context.translate(this.x, this.y);
+        context.rotate(this.angle);
+        Drawing.drawAsteroid(context, this.radius, this.#shape, this.#noise);
         context.restore();
     }
 
-
-    update(context: CanvasRenderingContext2D, timeElapsed: number) {
-        const {height, width} = context.canvas;
-        // Position:
-        this.#x += this.#speed.x * timeElapsed; // deltaX;
-        this.#y += this.#speed.y * timeElapsed; // deltaY;
-        // Right border:
-        if (this.#x - this.#radius > width) {
-            this.#x = -this.#radius;
-        }
-        // Left border:
-        if (this.#x + this.#radius < 0) {
-            this.#x = width + this.#radius;
-        }
-        // Top border:
-        if (this.#y + this.#radius < 0) {
-            this.#y = height + this.#radius;
-        }
-        // Bottom border:
-        if (this.#y - this.#radius > height) {
-            this.#y = -this.#radius;
-        }
-        // Rotation:
-        this.#angle = (this.#angle + this.#speed.rotation * timeElapsed) % (2 * Math.PI);
-    }
 
 }
