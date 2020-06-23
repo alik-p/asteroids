40730dc16f84564b7d476ed7ac142e8df454bf94
alik-p <aliaksandr.papko@gmaol.com>
2020-06-23 23:05:36 +0300

    refactor: inherit model `Spaceship` from `Mass`

--- src/game/spaceship/spaceship.model.ts
+++ src/game/spaceship/spaceship.model.ts
@@ -1,53 +1,20 @@
 import { SpaceshipDrawing as Drawing } from './spaceship-drawing';
+import { Mass } from '../shared/mass/mass.model';
 
 
-export class Spaceship {
-
-    readonly angle: number;
-
-    readonly #radius: number;
-    #x: number;
-    #y: number;
-    #speed = {x: 0, y: -50};
+export class Spaceship extends Mass {
 
     constructor(x: number, y: number, radius: number) {
-        this.#x = x;
-        this.#y = y;
-        this.angle = Math.PI * 1.5;
-        this.#radius = radius;
+        super(x, y, radius, 1.5 * Math.PI, {x: 0, y: -50, rotation: 0});
     }
 
 
     draw(context: CanvasRenderingContext2D): void {
         context.save();
-        context.translate(this.#x, this.#y);
+        context.translate(this.x, this.y);
         context.rotate(this.angle);
-        Drawing.drawSpaceship(context, this.#radius);
+        Drawing.drawSpaceship(context, this.radius);
         context.restore();
-    }
-
-
-    update(context: CanvasRenderingContext2D, timeElapsed: number) {
-        const {height, width} = context.canvas;
-        // Position:
-        this.#x += this.#speed.x * timeElapsed; // deltaX;
-        this.#y += this.#speed.y * timeElapsed; // deltaY;
-        // Right border:
-        if (this.#x - this.#radius > width) {
-            this.#x = -this.#radius;
-        }
-        // Left border:
-        if (this.#x + this.#radius < 0) {
-            this.#x = width + this.#radius;
-        }
-        // Top border:
-        if (this.#y + this.#radius < 0) {
-            this.#y = height + this.#radius;
-        }
-        // Bottom border:
-        if (this.#y - this.#radius > height) {
-            this.#y = -this.#radius;
-        }
     }
 
 }
